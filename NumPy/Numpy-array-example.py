8b7d8a7b1d9bd9561739073707d361ffde943d0b
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-07-28 21:19:24 +0530

    Added numpy arithmetic

--- NumPy/Numpy-array-example.py
+++ NumPy/Numpy-array-example.py
@@ -1,4 +1,6 @@
+#NumPy array
 # access values
+
 import numpy
 
 mylist = [[1, 2, 3], [3, 4, 5]]
@@ -13,3 +15,10 @@ print("Last row: ", myarray[-1])
 
 print("Specific row and column: ", myarray[0, 2])
 print("Whole column: ", myarray[:, 2])
+
+# arithmetic
+myarray1 = numpy.array([2, 2, 2])
+myarray2 = numpy.array([3, 3, 3])
+
+print("Addition: ", myarray1 + myarray2)
+print("Multiplication: ", myarray1 * myarray2)
