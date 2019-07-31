9105f9f40d740c9ea7a84f23c2a410ace32078fa
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-07-31 21:32:05 +0530

    Added dictionary example

---
+++ Dictionary/dictionary-exp-1.py
@@ -0,0 +1,28 @@
+# Creating an empty Dictionary 
+Dict = {} 
+print("Empty Dictionary: ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with Integer Keys 
+Dict = {1: 'Nitin', 2: 'Nilesh', 3: 'Nitesh'} 
+print("\nDictionary with the use of Integer Keys: ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with Mixed keys 
+Dict = {'Name': 'Nitin', 1: [1, 2, 3, 4]} 
+print("\nDictionary with the use of Mixed Keys: ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with dict() method 
+Dict = dict({1: 'book1', 2: 'book2', 3:'book3'}) 
+print("\nDictionary with the use of dict(): ") 
+print(Dict) 
+
+# Creating a Dictionary 
+# with each item as a Pair 
+Dict = dict([(1, 'C++'), (2, 'Python')]) 
+print("\nDictionary with each item as a pair: ") 
+print(Dict) 
