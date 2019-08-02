5db9e0a730cfe1457922efcb62702b8f53a79149
NitinJRepo <nitin.jatpuriya@gmail.com>
2019-08-02 22:42:30 +0530

    accessing elements from dictionary

---
+++ Dictionary/4-dictionary-accessing-elements.py
@@ -0,0 +1,18 @@
+# Python program to demonstrate 
+# accesing a element from a Dictionary 
+
+# Creating a Dictionary 
+Dict = {1: 'Hello', 'name': 'Nitin', 3: 'How are you'} 
+
+# accessing a element using key 
+print("Acessing a element using key:") 
+print(Dict[1]) 
+
+# accessing a element using key 
+print("Acessing a element using key:") 
+print(Dict['name']) 
+
+# accessing a element using get() 
+# method 
+print("Acessing a element using get:") 
+print(Dict.get(3)) 
